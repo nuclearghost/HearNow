c3307b2bad8500814e1cbcb49949c55f8637baa6
Mark Meyer <mark.meyer37@gmail.com>
2014-09-27 13:18:50 -0700

    Added a cell

---
+++ HearNow/ConcertCell.swift
@@ -0,0 +1,17 @@
+//
+//  ConcertCell.swift
+//  HearNow
+//
+//  Created by Mark Meyer on 9/27/14.
+//  Copyright (c) 2014 Mark Meyer + Ryan Loomba. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ConcertCell : UITableViewCell {
+    
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var venueLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+}
--- HearNow/HomeViewController.swift
+++ HearNow/HomeViewController.swift
@@ -9,9 +9,10 @@
 import Foundation
 import UIKit
 
-class HomeViewController: UITableViewController, UITextFieldDelegate, SongkickAPIProtocol {
+class HomeViewController: UITableViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate, SongkickAPIProtocol {
     
     var api: SongkickAPI = SongkickAPI()
+    var concerts: NSDictionary = NSDictionary()
 
     @IBOutlet weak var LocationSearchField: UITextField!
 
@@ -27,8 +28,69 @@ class HomeViewController: UITableViewController, UITextFieldDelegate, SongkickAP
         api.delegate = self
         api.pingSongkick()
     }
-
     
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1;
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell: ConcertCell! = self.tableView.dequeueReusableCellWithIdentifier("ConcertCell" ,forIndexPath: indexPath) as ConcertCell
+        
+        /*
+        rowData = dataArray[indexPath.row] as NSDictionary
+        var title=rowData["title"] as String
+        var subtitle=rowData["subtitle"] as String
+        var image=rowData["thumb"] as String
+        
+        cell.miaImmagine.alpha=0.0
+        cell.mioTesto.alpha=0.0
+        cell.mioSubtitle.alpha=0.0
+        
+        var imageUrl = NSURL(string: image)
+        var request = NSURLRequest(URL: imageUrl)
+        var requestQueue : NSOperationQueue = NSOperationQueue()
+        NSURLConnection.sendAsynchronousRequest(request, queue: requestQueue, completionHandler:
+            {(response: NSURLResponse!, responseData: NSData!, error: NSError!) -> Void in
+                if error != nil {
+                    println("error")
+                }
+                else {
+                    dispatch_async(dispatch_get_main_queue(), {
+                        cell.mioTesto.text = title
+                        cell.mioSubtitle.text = subtitle
+                        cell.miaImmagine.image=UIImage(data: responseData)
+                        UIView.animateWithDuration(1.0,
+                            delay: 0.0,
+                            options: .CurveEaseInOut,
+                            animations: {
+                                cell.miaImmagine.alpha=1.0
+                                cell.mioTesto.alpha=1.0
+                                cell.mioSubtitle.alpha=1.0
+                            },
+                            completion: { finished in
+                                
+                        })
+                        
+                    })
+                }
+        })
+        */
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        println("Selected: \(indexPath.row)!")
+        //performSegueWithIdentifier("miaview", sender: self.view)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    //mark LocationSearchField: UITextFieldDelegate
     func textFieldDidEndEditing(textField: UITextField) {
         println("Text field did end. Text \(self.LocationSearchField.text)")
         self.LocationSearchField.resignFirstResponder()
@@ -39,8 +101,8 @@ class HomeViewController: UITableViewController, UITextFieldDelegate, SongkickAP
         return true
     }
     
+    //mark SongkickAPI delegate
     func didRecieveResponse(results: NSDictionary) {
-        // Store the results in our table data array
         println(results)
     }
 }

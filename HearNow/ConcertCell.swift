4bc930c28f8a4c0cbf7f68ad9a152ca14af76775
Mark Meyer <mark.meyer37@gmail.com>
2014-09-27 16:01:58 -0700

    Getting local concerts. Added imageView

--- HearNow/ConcertCell.swift
+++ HearNow/ConcertCell.swift
@@ -14,4 +14,5 @@ class ConcertCell : UITableViewCell {
     @IBOutlet weak var artistLabel: UILabel!
     @IBOutlet weak var venueLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var imgView: UIImageView!
 }
